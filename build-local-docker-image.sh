f430b93af25df5a9d0885ae4004e0567a35b6f4f
Muhammad Ahsan Khan <56512649+mahsankhaan@users.noreply.github.com>
2024-04-20 20:59:14 +0500

    Update build-local-docker-image.sh

--- build-local-docker-image.sh
+++ build-local-docker-image.sh
@@ -33,7 +33,7 @@ function package_nocodb() {
 function build_image() {
     # build container image
    # buildah --storage-driver=overlay bud --no-cache -f ./tekton/Dockerfiles/Dockerfile.copying-artifacts -t nocodb-runner-image .  (our custom image)
-     buildah --storage-driver=overlay bud --no-cache -f${SCRIPT_DIR}/packages/nocodb/Dockerfile -t nocodb-runner-image .
+     buildah --storage-driver=overlay bud --no-cache -f ${SCRIPT_DIR}/packages/nocodb/Dockerfile -t nocodb-runner-image .
      buildah --storage-driver=overlay tag nocodb-runner-image selfhosted.jfrog.io/nocodb-deps/nocodb-runner-image:new
      buildah --storage-driver=overlay --creds tekton:Tekton123@ push selfhosted.jfrog.io/nocodb-deps/nocodb-runner-image:new
 }
